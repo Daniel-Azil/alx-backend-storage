cfb96c58b4c22e03b74ce4faf25da2e032457080
Daniel-Azil <danielazil,fse@gmail.com>
2024-06-18 13:54:59 -0700

    Modified doc string.

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" A module that returns chaneges on all topics of a school document based on the name"""
+"""
+    A module that returns chaneges on all topics of a school document based
+    on the name
+"""
 import pymongo
 
 def update_topics(mongo_collection, name, topics):
