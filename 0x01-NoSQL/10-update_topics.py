5ee5201c3320a64b35d4ae5272f0123f4bfd217b
Daniel-Azil <danielazil,fse@gmail.com>
2024-06-18 13:50:43 -0700

    Added return statement to the given function.

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,5 +5,5 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """A function that returns changes all topics of a school document
         based on the name"""
-     mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+     return mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
 
